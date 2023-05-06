232b2d64d8c4a5f65d820b5b8252f3e1edc5ad09
eahb0y <getelementby111@gmail.com>
2023-05-07 01:40:38 +0500

    text styles was added

---
+++ lib/constants.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+ TextStyle welcomeStyle = const TextStyle(
+fontWeight: FontWeight.w500,
+fontSize: 50,
+color: Colors.black87
+);
+
+ TextStyle spaceStyle = TextStyle(
+   fontSize: 70,
+   color: Colors.purple.shade600,
+   fontWeight: FontWeight.w500
+ );
+TextStyle cowStyle = TextStyle(
+    fontSize: 55,
+    color: Colors.purple.shade600,
+    fontWeight: FontWeight.w300
+);
