bb4935ed64e2b82ef687a8baf54fc96926448fce
eahb0y <getelementby111@gmail.com>
2023-05-07 01:08:10 +0500

    connected with welcome screen file and used widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cspace/screens/welcome_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,6 +10,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const MaterialApp(
+      home: WelcomeScreen(),
+    );
   }
 }
